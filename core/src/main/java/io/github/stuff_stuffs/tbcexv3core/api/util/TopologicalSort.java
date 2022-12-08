67a11c02199657e3a9583acbace14307069e0b12
Stuff-Stuffs <stevenziehlke@gmail.com>
2022-12-08 11:01:01 -0600

    TopologicalSort bug fix

--- core/src/main/java/io/github/stuff_stuffs/tbcexv3core/api/util/TopologicalSort.java
+++ core/src/main/java/io/github/stuff_stuffs/tbcexv3core/api/util/TopologicalSort.java
@@ -12,44 +12,21 @@ import java.util.Set;
 
 public final class TopologicalSort {
     public static <T> List<T> tieBreakingSort(final List<T> items, final ChildPredicate<T> childPredicate, final Comparator<T> tieBreaker) {
-        final Set<T> set = new ObjectOpenHashSet<>();
-        final int size = items.size();
-        final int[] indexToDependencyCount = new int[size];
-        for (int i = 0; i < size; i++) {
-            final T item = items.get(i);
-            if (!set.add(item)) {
-                throw new IllegalStateException("Duplicate items detected!");
-            }
-            for (int j = 0; j < size; j++) {
-                if (childPredicate.isChild(i, j, items)) {
-                    indexToDependencyCount[j] += 1;
-                }
-            }
+        if (items.isEmpty()) {
+            return List.of();
         }
         final IntPriorityQueue queue = new IntHeapPriorityQueue((i0, i1) -> tieBreaker.compare(items.get(i0), items.get(i1)));
-        final List<T> output = new ArrayList<>(size);
-        for (int i = 0; i < size; i++) {
-            if (queue.isEmpty()) {
-                throw new IllegalStateException("Cycle detected!");
-            }
-            final int idx = queue.dequeueInt();
-            final T item = items.get(idx);
-            output.add(item);
-            for (int j = 0; j < size; j++) {
-                if (childPredicate.isChild(i, j, items)) {
-                    if ((--indexToDependencyCount[j]) == 0) {
-                        queue.enqueue(j);
-                    }
-                }
-            }
-        }
-        return output;
+        return sort(items, childPredicate, queue, dependencyCount(items, childPredicate));
     }
 
     public static <T> List<T> sort(final List<T> items, final ChildPredicate<T> childPredicate) {
         if (items.isEmpty()) {
             return List.of();
         }
+        return sort(items, childPredicate, new IntArrayFIFOQueue(), dependencyCount(items, childPredicate));
+    }
+
+    private static <T> int[] dependencyCount(final List<T> items, final ChildPredicate<T> childPredicate) {
         final Set<T> set = new ObjectOpenHashSet<>();
         final int size = items.size();
         final int[] indexToDependencyCount = new int[size];
@@ -64,7 +41,11 @@ public final class TopologicalSort {
                 }
             }
         }
-        final IntPriorityQueue queue = new IntArrayFIFOQueue();
+        return indexToDependencyCount;
+    }
+
+    private static <T> List<T> sort(final List<T> items, final ChildPredicate<T> childPredicate, final IntPriorityQueue queue, final int[] indexToDependencyCount) {
+        final int size = items.size();
         for (int i = 0; i < size; i++) {
             if (indexToDependencyCount[i] == 0) {
                 queue.enqueue(i);
